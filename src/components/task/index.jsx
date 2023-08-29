992bbc02e886e1da2fcf107a1c0a990e81fce089
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-08-29 17:41:40 +0530

    UI Completed

--- src/components/task/index.jsx
+++ src/components/task/index.jsx
@@ -1,10 +1,15 @@
 import styles from './task.module.css'
+import { AiFillDelete } from "react-icons/ai";
 
 export function Task() {
     return (
         <div className={styles.task}>
             <button className={styles.checkContainer}>
-                <div></div>
+                <div />
+            </button>
+            <p>Molestiae pariatur provident, aliquam voluptatibus iure aliquid tempora necessitatibus mollitia excepturi!</p>
+            <button className={styles.delBtn}>
+                <AiFillDelete size={28} />
             </button>
         </div>
     )
